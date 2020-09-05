fd48516ae4701d756fc69a393ec86614441b6311
kurumi <563693600@qq.com>
2020-09-05 23:45:55 +0800

    add create taro3 page

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,12 @@ import commander from 'commander';
 import { wxRoute } from './taro_route';
 import { flutterImage } from './flutter_image';
 import { reactImage } from './react_image';
+import { generateFile } from './taro_file';
 
 commander
-  .command('wx')
+  .command('wxRoute')
   .option('-p, --path <path>', '查找路径,default to src\\pages', 'src\\pages')
-  .description('start command description')
+  .description('自动生成微信小程序pages路由')
   .action((cmd) => {
     // wxRoute()
     console.log('使用规则，分包的文件名必须末尾Package，页面名称末尾Page');
@@ -20,19 +21,31 @@ commander
   });
 
 commander
-  .command('flutter')
-  .option('-p, --path <>', '查找路径,default to assets', 'assets')
-  .description('start command description')
+  .command('image')
+  .option('-p, --path <>', '查找路径,default to assets',)
+  .option('-t, --target <>', 'fluter or taro', '')
+  .description('生成fluter or taro Image类')
   .action((cmd) => {
-    console.log(cmd.path, 'ooooooooooooooo');
-    flutterImage(cmd.path);
+    const target = cmd.target;
+    if (!cmd.target) {
+      throw new Error("target option is required");      
+    }
+    if (target === 'flutter') {
+      flutterImage(cmd.path ?? 'asstes');
+    } else {
+      reactImage(cmd.path ?? 'src\\assets');
+    }
+    
   });
 
 commander
-  .command('react')
-  .option('-p, --path <>', '查找路径,default to src\\assets', 'src\\assets')
-  .description('start command description')
+  .command('taro')
+  .option('-n, --filename <>', '文件名', '.\\')
+  .option('-c --config <>', '是否生成config文件,默认为true', true)
+  .description('自动生成taro3页面文件')
   .action((cmd) => {
-    reactImage(cmd.path);
+    const {filename, config} = cmd;
+
+    generateFile((filename as string).substring(1), config);
   });
 commander.parse(process.argv);
---
+++ src/taro_file.ts
@@ -0,0 +1,32 @@
+import fs from 'fs';
+
+const configTs = `
+export default {
+    navigationBarTitleText: '标题',
+}
+`;
+
+const widgetTx = `
+import React, { FC } from 'react';
+import { View } from '@tarojs/components';
+
+import './{0}.scss';
+
+const {0}: FC = ({}) => {
+    return <View >{0}</View>;
+}
+
+export default {0};
+`;
+
+export function generateFile(fileName: string, isConfig: boolean) {
+    const str = widgetTx.replace(/\{0\}/g, fileName);
+    if (!fs.existsSync(`${process.cwd()}/${fileName}`)) {
+        fs.mkdirSync(`${process.cwd()}/${fileName}`)
+    };
+    fs.writeFileSync(`${process.cwd()}/${fileName}/${fileName}.tsx`, str);
+    fs.writeFileSync(`${process.cwd()}/${fileName}/${fileName}.scss`, '');
+    if (isConfig) {
+        fs.writeFileSync(`${process.cwd()}/${fileName}/${fileName}.config.ts`, configTs);
+    }
+}
